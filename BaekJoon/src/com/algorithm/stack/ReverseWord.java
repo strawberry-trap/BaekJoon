9bdfb82332846c11e4e8428d376e00ac8713df61
Goinmul <21400045@handong.edu>
2020-11-28 20:42:48 +0900

    #9093 ReverseWord, updated from using 'Scanner' to 'BufferedReader' for performance

--- BaekJoon/src/com/algorithm/stack/ReverseWord.java
+++ BaekJoon/src/com/algorithm/stack/ReverseWord.java
@@ -1,4 +1,7 @@
 package com.algorithm.stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -11,6 +14,7 @@ easy
 */
 public class ReverseWord {
 	
+	// 1. Scanner
 	public void solution() {
 		
 		Scanner scanner = new Scanner(System.in);
@@ -22,11 +26,9 @@ public class ReverseWord {
 		for (int i=0; i<testCases; i++) {
 
 			String line = scanner.nextLine() + " ";
-			
 			for (int j=0; j<line.length(); j++) {
 				
 				char c = line.charAt(j);
-				
 				if (c == ' ') {
 					while(!stack.isEmpty()) {
 						System.out.print(stack.pop());
@@ -36,10 +38,39 @@ public class ReverseWord {
 					stack.push(c);
 				}
 			}
-			
 			System.out.println();
 		}
 		scanner.close();
+		return;
+	}
+	
+	// 2. BufferedReader
+	public void secondSolution() throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testCases = Integer.parseInt(br.readLine());
+		//br.readLine(); // for ignoring '\n' at the end of first line
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for (int i=0; i<testCases; i++) {
+
+			String line = br.readLine() + " ";
+			for (int j=0; j<line.length(); j++) {
+				
+				char c = line.charAt(j);
+				if (c == ' ') {
+					while(!stack.isEmpty()) {
+						System.out.print(stack.pop());
+					}
+					System.out.print(" ");
+				} else {
+					stack.push(c);
+				}
+			}
+			System.out.println();
+		}
+		br.close();
 		return;
 	}
 }
