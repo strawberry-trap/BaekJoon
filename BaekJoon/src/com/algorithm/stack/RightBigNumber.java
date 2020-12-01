8c5faf355d6fff93472e96cab425a1e8077f9d7f
Goinmul <21400045@handong.edu>
2020-12-01 23:52:03 +0900

    #17298 duplicate condition check removed

--- BaekJoon/src/com/algorithm/stack/RightBigNumber.java
+++ BaekJoon/src/com/algorithm/stack/RightBigNumber.java
@@ -14,7 +14,7 @@ public class RightBigNumber {
 		
 		// read input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));//¼±¾ð
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		Stack<Integer> stack = new Stack<>();
 		
 		String firstLine = br.readLine(); // assume numbers > 0
@@ -23,18 +23,16 @@ public class RightBigNumber {
 		String secondLine = br.readLine();
 		String[] tempArray = secondLine.split(" ");
 		int[] array = new int[numbers]; // store numbers in array
+		int[] answer = new int[numbers]; // store each number's right-big-number
+		
 		for (int k=0; k<tempArray.length; k++) {
 			array[k] = Integer.parseInt(tempArray[k]);
 		}
 		
-		int[] answer = new int[numbers]; // store each number's right-big-number
-		
 		stack.push(0); // at the beginning, push index 0
 		for (int i=1; i<numbers; i++) {
-			if (array[i] > array[stack.peek()]) { // it means that array[i] is right-big-number of the remaining numbers in stack
-				while(!stack.isEmpty() && array[stack.peek()] < array[i]) {
-					answer[stack.pop()] = array[i];
-				}
+			while(!stack.isEmpty() && array[stack.peek()] < array[i]) { // it means that array[i] is right-big-number of the remaining numbers in stack
+				answer[stack.pop()] = array[i];
 			}
 			stack.push(i);
 		}
@@ -49,8 +47,8 @@ public class RightBigNumber {
 		}
 		
 		bw.flush();
-		bw.close();
-		br.close();
+//		bw.close();
+//		br.close();
 		
 		return;
 	}
