2b6ff5c65def372684b624c5ab0912a91aba78d6
Goinmul <21400045@handong.edu>
2021-02-01 22:07:09 +0900

    #16955 Gomoku Game, brute force (working on)

---
+++ BaekJoon/src/com/algorithm/bruteforce/GomokuGame.java
@@ -0,0 +1,76 @@
+package com.algorithm.bruteforce;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class GomokuGame {
+
+	public static final int BOARD_LENGTH = 10;
+	public static final int DIRECTIONS = 8;
+	public static final int WINNING_STONES = 5;
+	public static char[][] board = new char[BOARD_LENGTH][BOARD_LENGTH];
+
+	public void main() throws IOException {
+
+		// read input
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<int[]> stone = new ArrayList<int[]>();
+
+		for (int i=0; i<BOARD_LENGTH; i++) {
+
+			String row = bf.readLine();
+			for (int j=0; j<row.length(); j++) {
+				board[i][j] = row.charAt(j);
+
+				// if there is a stone, save that coordinate
+				if (board[i][j] == 'X') {
+					stone.add(new int[] {i, j});
+				}
+			}
+		}
+
+		// 8 directions that pairs like dx[i], dx[i]
+		int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1};
+		int[] dy = {-1, -1, -1, 0, 1, 1, 1, 0};
+		
+		// pick a stone
+		for (int j=0; j<stone.size(); j++) {
+
+			int[] starting = stone.get(j);
+			int x = starting[0];
+			int y = starting[1];
+			
+			// for each directions,
+			for (int i=0; i<DIRECTIONS; i++) {
+				
+				// check if we could put 5 stones in a row. (loop runs 4 times)
+				int cnt = 1;
+				for (int k=0; k<WINNING_STONES-1; k++) {
+					
+					x += dx[i];
+					y += dy[i];
+					if (x>=0 && x<BOARD_LENGTH && y>=0 && y<BOARD_LENGTH) {
+						
+						if (board[x][y] == 'X') {
+							cnt++;
+						}
+						else if (board[x][y] == 'O') // blocked, no need to search further
+							break;
+					}
+				}
+				
+				if (cnt == 4) {
+					System.out.println(1);
+					return;
+				}
+			}
+		}
+		
+		System.out.println(0);
+		bf.close();	
+		return;
+	}
+}
